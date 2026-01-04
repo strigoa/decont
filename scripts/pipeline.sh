2d7728f970858483845dad516d28d1fe8eed4228
Sofia Trigo <sofia.trigo.anca@gmail.com>
2026-01-04 12:50:33 +0100

    Continued with pipeline.sh - cutadapt for merged files. Also changed the delimitator in merged files part to -

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -12,9 +12,36 @@ bash scripts/download.sh https://bioinformatics.cnio.es/data/courses/decont/cont
 bash scripts/index.sh res/contaminants.fasta res/contaminants_idx
 
 # Merge the samples into a single file
-for sid in $(ls data/*.fastq.gz | xargs -n 1 basename | cut -d'.' -f1 | sort | uniq) 
+for sid in $(ls data/*.fastq.gz | xargs -n 1 basename | cut -d'-' -f1 | sort | uniq) 
 do
     bash scripts/merge_fastqs.sh data out/merged $sid
 done
 
+# Run cutadapt for all merged files
+mkdir -p log/cutadapt
+mkdir -p out/trimmed
+for mergedsample in $(ls out/merged/*.fastq.gz | xargs -n 1 basename | cut -d'.' -f1 | sort | uniq) 
+do
+	echo "Running cutadapt for sample $mergedsample..."
+	cutadapt -m 18 -a TGGAATTCTCGGGTGCCAAGG --discard-untrimmed \
+	-o out/trimmed/${mergedsample}.trimmed.fastq.gz \
+	out/merged/${mergedsample}.fastq.gz > log/cutadapt/${mergedsample}.log
+done
+
+# TODO: run STAR for all trimmed files
+for fname in out/trimmed/*.fastq.gz
+do
+    # you will need to obtain the sample ID from the filename
+    sid=#TODO
+    # mkdir -p out/star/$sid
+    # STAR --runThreadN 4 --genomeDir res/contaminants_idx \
+    #    --outReadsUnmapped Fastx --readFilesIn <input_file> \
+    #    --readFilesCommand gunzip -c --outFileNamePrefix <output_directory>
+done 
+
+# TODO: create a log file containing information from cutadapt and star logs
+# (this should be a single log file, and information should be *appended* to it on each run)
+# - cutadapt: Reads with adapters and total basepairs
+# - star: Percentages of uniquely mapped reads, reads mapped to multiple loci, and to too many loci
+# tip: use grep to filter the lines you're interested in
 
